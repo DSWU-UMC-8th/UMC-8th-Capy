b81a4b2f2325e54b298a0a4a405f994b7d44a5ea
한지수 <eldeoddt@gmail.com>
2025-05-05 01:04:31 +0900

    [FEAT] 5주차 미션
    양방향 매핑

---
+++ src/main/java/umc/spring/domain/ReviewImage.java
@@ -0,0 +1,27 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.MemberAgree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewImage extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String image_url;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
